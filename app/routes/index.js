10340e791e15987840b8300092b6f9ed5c4dfd12
Rémi <remi.chaignon@gmail.com>
2013-07-28 22:49:19 -0600

    Index route

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,1 +1,11 @@
-MY_APP.IndexRoute = MY_APP.Route.extend({});
+MY_APP.IndexRoute = MY_APP.Route.extend({
+	// Events
+	events: {
+		error: function () {}
+	},
+
+	// Hooks
+	beforeModel: function () {
+		this.setup();
+	}
+});
